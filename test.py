94eb27a488d9eb2692e5cb305d9ae8c2f6c0fd33
intersalad <140984613+intersalad@users.noreply.github.com>
2023-12-27 20:25:01 +0300

    poisk

---
+++ test.py
@@ -0,0 +1,5 @@
+a = {'who_rec': [('Владислав', 'Волков', 172014705), ('Юлия', 'Тарасова', 311085142)]}
+
+for specialist in a['who_rec']:
+    print(specialist[2])
+    print(specialist[0] + ' ' + specialist[1])
